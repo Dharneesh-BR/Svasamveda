95b48b57ead57c5a96a3b833f0232778be8600e7
Adithya N Kashyap <balajiadithya2003@gmail.com>
2025-07-30 12:13:03 +0530

    chore: add terser as dev dependency

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,8 @@
-// Explicitly require the plugins
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
 module.exports = {
   plugins: [
-    // Some plugins, like postcss-nested, need to run before Tailwind
     require('postcss-import'),
-    
-    // Tailwind CSS
-    tailwindcss,
-    
-    // Autoprefixer should be last in the chain
-    autoprefixer,
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
   ],
 };
