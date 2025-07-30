56ed43927a852df76dec9c3cfdff3f4f120772f4
Adithya N Kashyap <balajiadithya2003@gmail.com>
2025-07-30 09:21:26 +0530

    fix: update PostCSS config and add required dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+// Explicitly require the plugins
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    // Some plugins, like postcss-nested, need to run before Tailwind
+    require('postcss-import'),
+    
+    // Tailwind CSS
+    tailwindcss,
+    
+    // Autoprefixer should be last in the chain
+    autoprefixer,
+  ],
 };
